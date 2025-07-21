9e8755fdc14b19b994b63d79eea54e05f73f34a8
subh077 <88318416+subarna0077@users.noreply.github.com>
2025-07-21 18:33:36 +0545

    Implemented the concepts of relations

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -5,16 +5,28 @@ from django.utils.text import slugify
 
 # Create your models here.
 
+class Country(models.Model):
+    country= models.CharField(max_length=60)
+    code = models.CharField(max_length=2)
+
+    def __str__(self):
+        return f"{self.country} {self.code}"
+
+    class Meta:
+        verbose_name_plural = "Countries Entries"    
+
+    
+
 
 class Address(models.Model):
     street = models.CharField(max_length = 80)
     postal_code = models.CharField(max_length=5)
     city = models.CharField(max_length=50)
-
     def __str__(self):
         return f"{self.street} {self.city}"
     
-
+    class Meta:
+        verbose_name_plural = "Address Entries"    
 
 
 class Author(models.Model):
@@ -25,6 +37,7 @@ class Author(models.Model):
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
     
+    
 
 
 
@@ -34,6 +47,7 @@ class Book(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True, related_name="Books")
     is_bestselling = models.BooleanField(default=False)
     slug= models.SlugField(default="" , blank=True, null=False, db_index=True) 
+    published_country = models.ManyToManyField(Country)
 
     def __str__(self):
         return f"{self.title} ({self.rating})"
