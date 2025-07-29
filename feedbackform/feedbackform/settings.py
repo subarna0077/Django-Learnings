d3c0727420a1c7720c9163b7305457c0f7773aa7
subh077 <88318416+subarna0077@users.noreply.github.com>
2025-07-29 18:11:00 +0545

    Learned the concepts of file uploads and serving the uploaded file

--- feedbackform/feedbackform/settings.py
+++ feedbackform/feedbackform/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'reviews',
+    'practicefeedback',
+    'profiles'
 ]
 
 MIDDLEWARE = [
@@ -121,3 +123,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_ROOT = BASE_DIR / "uploads"
+MEDIA_URL = "/user-media/"
